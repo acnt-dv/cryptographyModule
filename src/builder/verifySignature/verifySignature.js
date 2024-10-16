ee92959647d5dbbba60d8c25203145702e39faf0
d.sajedi <d.sajedi@fanap.ir>
2024-10-16 11:44:09 +0330

    [implement] - implementation of verification of signature.

---
+++ src/builder/verifySignature/verifySignature.js
@@ -0,0 +1,18 @@
+import {createVerify} from 'crypto';
+
+/**
+ *k
+ * @param data
+ * @param publicKey
+ * @param signature
+ * @param algorithm
+ *
+ * @returns {boolean} A buffer containing the digital signature of the data.
+ */
+export default (data, publicKey, signature, algorithm='SHA256') => {
+    const verify = createVerify(algorithm);
+    verify.update(data);
+    verify.end();
+
+    return verify.verify(publicKey, signature, 'base64');
+}
